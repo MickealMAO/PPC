a7bdc6ed82336abfe292d1185b820d992732544f
Yichao MAO <mickeal.mao@gmail.com>
2024-01-28 00:35:16 +0100

    test socket

--- game_server.py
+++ game_server.py
@@ -1,102 +1,68 @@
 import socket
-import queue
-import os
-import signal
-import random
-from threading import Thread, Lock
-from multiprocessing import Process, Manager
+import concurrent.futures
+import select
 
-import IPC
-import player as pl
-import Hannabis as H
+HOST = "localhost"
+PORT = 1789
 
-class Card:
-    def __init__(self, color, number):
-        self.color = color
-        self.number = number
+def socket_handler(client_socket, address):
+    try:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            response = "Your response here"
+            client_socket.sendall(response.encode())
+    finally:
+        client_socket.close()
 
-    def __repr__(self):
-        return f"{self.color}{self.number}"
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    server_socket.setblocking(False)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(4)
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        connections = []
+        while True:
+            readable, _, _ = select.select([server_socket] + connections, [], [])
+            for s in readable:
+                if s is server_socket:
+                    client_socket, address = server_socket.accept()
+                    client_socket.setblocking(False)
+                    connections.append(client_socket)
+                else:
+                    executor.submit(socket_handler, s, address)
+import socket
+import concurrent.futures
+import select
 
-def create_deck(number_of_players):
-    colors = ['Red', 'Blue', 'Green', 'Yellow', 'White'][:number_of_players]
-    deck = [Card(color, number) for color in colors for number in [1, 1, 1, 2, 2, 3, 3, 4, 4, 5]]
-    random.shuffle(deck)
-    return deck
+HOST = "localhost"
+PORT = 1789
 
-class GameServer(Process):
-    def __init__(self, shared_tokens, shared_hands, played_cards):
-        super().__init__()
-        self.shared_tokens = shared_tokens
-        self.shared_hands = shared_hands
-        self.played_cards = played_cards
-        self.server_socket = IPC.create_server_socket('localhost', 8000)
-        self.player_connections = []
-        self.lock = Lock()
+def socket_handler(client_socket, address):
+    try:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            response = "Your response here"
+            client_socket.sendall(response.encode())
+    finally:
+        client_socket.close()
 
-class GameServer(Process):
-    def __init__(self, shared_tokens, shared_hands, played_cards):
-        super().__init__()
-        self.shared_tokens = shared_tokens
-        self.shared_hands = shared_hands
-        self.played_cards = played_cards
-        self.server_socket = IPC.create_server_socket('localhost', 8000)
-        self.player_connections = []
-
-    def run(self):
-        # 创建并启动玩家进程
-        self.player_processes = [pl.Player(player_id, self.shared_tokens, self.shared_hands, self.played_cards) for player_id in range(len(self.shared_hands))]
-        for player_process in self.player_processes:
-            player_process.start()
-            client_socket, _ = IPC.accept_client_connection(self.server_socket)
-            self.player_connections.append(client_socket)
-
-        # 为每个玩家创建一个线程来处理其动作
-        player_threads = []
-        for client_socket in self.player_connections:
-            player_thread = Thread(target=self.handle_player_connection, args=(client_socket,))
-            player_threads.append(player_thread)
-            player_thread.start()
-
-        # 等待所有线程完成
-        for player_thread in player_threads:
-            player_thread.join()
-
-        # 游戏结束，关闭所有连接
-        for client_socket in self.player_connections:
-            client_socket.close()
-        for player_process in self.player_processes:
-            player_process.join()
-
-    def handle_player_connection(self, client_socket):
-        while not self.is_game_over():
-            data = IPC.receive_data(client_socket)
-            if data:
-                # TODO: 根据接收到的数据处理玩家动作
-                # 例如: self.handle_player_action(data)
-
-                # TODO: 更新游戏状态
-                # 例如: self.update_game_state()
-                with self.lock:
-                    pass
-
-                # 将更新后的游戏状态发送给所有玩家
-                # TODO: 格式化游戏状态数据
-                # 例如: game_state_data = self.format_game_state()
-                for client_socket in self.player_connections:
-                    IPC.send_data(client_socket, b'Update or Response')
-
-
-    def is_game_over(self):
-        # number of fives played
-        num_fives = sum(1 for card in self.played_cards if card.number == 5)
-        # win condition
-        if num_fives == len(self.shared_hands):
-            print("Game Won - All 5s have been played successfully.")
-            return True
-        # lose condition
-        if self.shared_tokens['fuse_tokens'] <= 0:
-            print("Game Over - All fuse tokens used up.")
-            return True
-        return False
-
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    server_socket.setblocking(False)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(4)
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        connections = []
+        while True:
+            readable, _, _ = select.select([server_socket] + connections, [], [])
+            for s in readable:
+                if s is server_socket:
+                    client_socket, address = server_socket.accept()
+                    client_socket.setblocking(False)
+                    connections.append(client_socket)
+                else:
+                    executor.submit(socket_handler, s, address)
